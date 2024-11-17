833db796f03c1ce888fd52c7d49cd5dd77c15489
박진완 <>
2024-11-17 14:40:11 +0900

    feat(CommonComponents): :sparkles: 버튼 컴포넌트 1차 완성 (이미지 경로 색상별 수정 필요)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./components/Button";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -19,18 +20,23 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Button variant="primary" size="sm" icon="r-arrow" onClick={() => {}} children={"테스트 버튼"} />
+      <Button variant="primary" size="md" icon="r-arrow" onClick={() => {}} children={"테스트 버튼"} />
+      <Button variant="primary" size="lg" icon="r-arrow" onClick={() => {}} children={"테스트 버튼"} disabled />
+      <Button variant="primary" size="xl" icon="r-arrow" onClick={() => {}} children={"테스트 버튼"} />
+
+      <Button variant="secondary" size="sm" icon="search" onClick={() => {}} children={"테스트 버튼"} />
+      <Button variant="secondary" size="md" icon="search" onClick={() => {}} children={"테스트 버튼"} disabled />
+      <Button variant="secondary" size="lg" icon="search" onClick={() => {}} children={"테스트 버튼"} />
+      <Button variant="secondary" size="xl" icon="search" onClick={() => {}} children={"테스트 버튼"} />
     </>
   );
 }
 
-export default App
+export default App;
