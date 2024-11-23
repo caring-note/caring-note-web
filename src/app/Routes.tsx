2424422e2be49f23afe8c4f74cda45b5499bb365
ohoCoding <youngho7482@gmail.com>
2024-11-23 14:01:58 +0900

    feat(공통): ✨ 라우터 파일 분리 세팅

---
+++ src/app/Routes.tsx
@@ -0,0 +1,60 @@
+import { Navigate, RouteObject, useRoutes } from "react-router-dom";
+import ErrorPage from "../pages/ErrorPage";
+import Root from "../pages/Root";
+import Home from "../pages/Home";
+import Consult from "../pages/consult/Consult";
+import PastConsult from "../pages/consult/PastConsult";
+import ConsultCard from "../pages/consult/ConsultCard";
+import MedicineMemo from "../pages/consult/MedicineMemo";
+import MedicineConsult from "../pages/consult/MedicineConsult";
+import DiscardMedicine from "../pages/consult/DiscardMedicine";
+
+const Routes = () => {
+
+    const rootRoutes: RouteObject = {
+        path: "/",
+        element: <Navigate to="/main"/>,
+        errorElement: <ErrorPage />
+    };
+
+    const noMatchRoutes: RouteObject = {
+        path: "*",
+        element: <Navigate to="/main"/>,
+        errorElement: <ErrorPage />,
+    };
+
+    const mainRoutes: RouteObject = {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: 'main',
+                element: <Home/>
+            },
+        ]
+    }
+
+    const consultRoutes: RouteObject = {
+        path: "/",
+        element: <Root />,
+        children: [
+            { 
+              path: "consult", element: <Consult/>,
+              children: [
+                { index: true, element : <PastConsult /> },
+                { path :"pastConsult", element : <PastConsult /> },
+                { path :"consultCard", element : <ConsultCard /> },
+                { path :"medicineMemo", element : <MedicineMemo /> },
+                { path :"medicineConsult", element : <MedicineConsult /> },
+                { path :"discardMedicine", element : <DiscardMedicine /> },
+              ]
+            }
+        ]
+    }
+
+    const routes = [rootRoutes, noMatchRoutes, mainRoutes, consultRoutes];
+
+    return useRoutes(routes);
+}
+export default Routes;
