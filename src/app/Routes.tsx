1f2ba548e34d9d4ad2ab32315cea3f738b153e89
ohoCoding <youngho7482@gmail.com>
2024-11-23 14:59:22 +0900

    feat(기초상담화면): ✨ 라우터 & 기본 화면 설정

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -8,53 +8,81 @@ import ConsultCard from "../pages/consult/ConsultCard";
 import MedicineMemo from "../pages/consult/MedicineMemo";
 import MedicineConsult from "../pages/consult/MedicineConsult";
 import DiscardMedicine from "../pages/consult/DiscardMedicine";
+import AssistantRoot from "../pages/assistant/AssistantRoot";
+import AssistantHome from "../pages/assistant/AssistantHome";
+import AssistantBasicInfo from "../pages/assistant/AssistantBasicInfo";
+import AssistantHealthInfo from "../pages/assistant/AssistantHealthInfo";
+import AssistantLifeInfo from "../pages/assistant/AssistantLifeInfo";
 
 const Routes = () => {
+  const rootRoutes: RouteObject = {
+    path: "/",
+    element: <Navigate to="/main" />,
+    errorElement: <ErrorPage />,
+  };
 
-    const rootRoutes: RouteObject = {
-        path: "/",
-        element: <Navigate to="/main"/>,
-        errorElement: <ErrorPage />
-    };
+  const noMatchRoutes: RouteObject = {
+    path: "*",
+    element: <Navigate to="/main" />,
+    errorElement: <ErrorPage />,
+  };
 
-    const noMatchRoutes: RouteObject = {
-        path: "*",
-        element: <Navigate to="/main"/>,
-        errorElement: <ErrorPage />,
-    };
+  const mainRoutes: RouteObject = {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "main",
+        element: <Home />,
+      },
+    ],
+  };
 
-    const mainRoutes: RouteObject = {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
+  const consultRoutes: RouteObject = {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "consult",
+        element: <Consult />,
         children: [
-            {
-                path: 'main',
-                element: <Home/>
-            },
-        ]
-    }
+          { index: true, element: <PastConsult /> },
+          { path: "pastConsult", element: <PastConsult /> },
+          { path: "consultCard", element: <ConsultCard /> },
+          { path: "medicineMemo", element: <MedicineMemo /> },
+          { path: "medicineConsult", element: <MedicineConsult /> },
+          { path: "discardMedicine", element: <DiscardMedicine /> },
+        ],
+      },
+    ],
+  };
 
-    const consultRoutes: RouteObject = {
-        path: "/",
-        element: <Root />,
+  const assistantRoutes: RouteObject = {
+    path: "/",
+    element: <AssistantRoot />,
+
+    children: [
+      {
+        path: "assistant",
+        element: <AssistantHome />,
         children: [
-            { 
-              path: "consult", element: <Consult/>,
-              children: [
-                { index: true, element : <PastConsult /> },
-                { path :"pastConsult", element : <PastConsult /> },
-                { path :"consultCard", element : <ConsultCard /> },
-                { path :"medicineMemo", element : <MedicineMemo /> },
-                { path :"medicineConsult", element : <MedicineConsult /> },
-                { path :"discardMedicine", element : <DiscardMedicine /> },
-              ]
-            }
-        ]
-    }
+          { index: true, element: <AssistantBasicInfo /> },
+          { path: "basicInfo", element: <AssistantBasicInfo /> },
+          { path: "healthInfo", element: <AssistantHealthInfo /> },
+          { path: "lifeInfo", element: <AssistantLifeInfo /> },
+        ],
+      },
+    ],
+  };
+  const routes = [
+    rootRoutes,
+    noMatchRoutes,
+    mainRoutes,
+    consultRoutes,
+    assistantRoutes,
+  ];
 
-    const routes = [rootRoutes, noMatchRoutes, mainRoutes, consultRoutes];
-
-    return useRoutes(routes);
-}
+  return useRoutes(routes);
+};
 export default Routes;
--- src/pages/assistant/AssistantHome.tsx
+++ src/pages/assistant/AssistantHome.tsx
@@ -1,10 +1,52 @@
-import React from "react";
+import arrowHeadLeftGray from "@icon/arrowHeadLeftGray.png";
+import { changeActiveTab } from "../../reducers/tabReducer";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../hooks";
+import Button from "../../components/Button";
 
-const AssistantHome: React.FC = () => {
+const tabTitle = (text: string, goPage: string) => {
+  const navigate = useNavigate();
+  const activeTab = useAppSelector((state) => state.tab.activeTab);
+  const dispatch = useAppDispatch();
   return (
-    <div className="bg-black">
-      <h1>Welcome to the Assistant Home Page</h1>
-      <p>This is the Home component of the application.</p>
+    <p
+      className={`${
+        activeTab === goPage
+          ? "text-md font-extrabold text-blue-500 border-b-2 border-blue-500"
+          : "text-md font-extrabold text-gray-600"
+      } mr-10 hover:text-blue-500 hover:border-b-2 border-blue-500 cursor-pointer`}
+      onClick={() => {
+        dispatch(changeActiveTab(goPage));
+        navigate(goPage);
+      }}>
+      {text}
+    </p>
+  );
+};
+const AssistantHome = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-start w-full h-fit bg-gray-0 px-8 py-4">
+        <div className="flex flex-row justify-start w-full h-8 mt-4">
+          <img
+            src={arrowHeadLeftGray}
+            alt="arrowHeadLeftGray"
+            className="w-6 h-6"
+          />
+        </div>
+        <div className="flex flex-row items-center justify-start w-full h-8 mt-4 pl-6">
+          <p className="text-4xl text-black font-black">상담 카드 작성</p>
+          <Button _class="ml-4" variant="primary" onClick={() => {}}>
+            기록완료
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-start w-full h-14 pl-14 my-0 border-t-2 border-gray-200 border-b-2 border-b-gray-300">
+        {tabTitle("기본 정보", "/assistant/basicInfo")}
+        {tabTitle("건강 정보", "/assistant/healthInfo")}
+        {tabTitle("생활 정보", "/assistant/lifeInfo")}
+      </div>
+      <Outlet />
     </div>
   );
 };
