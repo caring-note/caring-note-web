d7890ae98c6a8b5eb1ceebc187d1a0fc04aab2ca
ohoCoding <youngho7482@gmail.com>
2024-11-29 23:44:28 +0900

    feat(기초상담화면): ✨ develop waringInfoBlue using Badge

--- src/pages/assistant/AssistantBasicInfo.tsx
+++ src/pages/assistant/AssistantBasicInfo.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "../../../hooks";
 import { changeActiveTab } from "../../reducers/tabReducer";
+import Badge from "../../components/Badge";
 
 const AssistantBasicInfo = () => {
   // 새로고침이 되었을 때도 active tab 을 잃지 않도록 컴포넌트 load 시 dispatch
@@ -8,6 +9,18 @@ const AssistantBasicInfo = () => {
   useEffect(() => {
     dispatch(changeActiveTab("/assistant/basicInfo")); // 해당 tab의 url
   }, []);
-  return <div>기본 정보 탭</div>;
+  return (
+    <div className="flex flex-col items-start justify-center bg-white w-full px-10 pt-5">
+      <Badge
+        _class=""
+        variant="primary"
+        size="xl"
+        type="tint"
+        text="이전 상담 노트에서 불러온 정보를 토대로 손쉽게 작성해보세요"
+        isIcon={true}
+        isWarning={true}
+      />
+    </div>
+  );
 };
 export default AssistantBasicInfo;
