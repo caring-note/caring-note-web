ebc16101bdd37a005f9f42c568b6744db95c6ad6
박진완 <50562139+berrycracker5@users.noreply.github.com>
2024-12-07 17:15:19 +0900

    Feature: 기타 수정 사항 반영 (#26)
    * feat: :lipstick: 왼쪽 네비게이션에 필요한 Icon png 추가 및 반영
    
    * feat: :lipstick: 본상담 상단 "임시저장" 버튼 추가
    
    * fix: :lipstick: 왼쪽 네비게이션의 하단메뉴들 스크롤이 생겨도 bottom으로 가도록 처리
    
    ---------
    
    Co-authored-by: 박진완

--- src/components/NavigationLeft.tsx
+++ src/components/NavigationLeft.tsx
@@ -1,31 +1,68 @@
 import React from "react";
-import adminBlack from "../assets/icon/adminBlack.png";
-import homeBlack from "../assets/icon/homeBlack.png";
-import homeBlue from "../assets/icon/homeBlue.png";
-import noteBlack from "../assets/icon/noteBlack.png";
-import questionBlack from "../assets/icon/questionBlack.png";
-import settingBlack from "../assets/icon/settingBlack.png";
+import adminBlack from "@icon/adminBlack.png";
+import adminBlue from "@icon/adminBlue.png";
+import homeBlack from "@icon/homeBlack.png";
+import homeBlue from "@icon/homeBlue.png";
+import noteBlack from "@icon/noteBlack.png";
+import questionBlack from "@icon/questionBlack.png";
+import settingBlack from "@icon/settingBlack.png";
 import logoBlack from "../assets/logoBlack.png";
 import Button from "../components/Button";
 import NavigationLeftMenu from "../components/NavigationLeftMenu";
+import noteBlue from "@icon/noteBlue.png";
+import paperPlaneBlack from "@icon/paperPlaneBlack.png";
+import paperPlaneBlue from "@icon/paperPlaneBlue.png";
 
 interface NavigationLeftProps {}
 
 const NavigationLeft: React.FC<NavigationLeftProps> = ({}) => {
   return (
-    <div className="w-64 h-screen relative py-4 bg-gray-0 border-r border-gray-300 z-1000">
+    <div className="w-64 h-auto relative py-4 z-1000">
       <div className="flex justify-center items-center">
         <img src={logoBlack} alt="logo" className="" />
       </div>
       <div className="flex justify-center items-center">
-        <Button _class="mb-5" variant="primary" size="xl" icon="none" onClick={() => {}} children={"박진완 약사님"} />
+        <Button
+          _class="mb-5"
+          variant="primary"
+          size="xl"
+          icon="none"
+          onClick={() => {}}
+          children={"박진완 약사님"}
+        />
       </div>
-      <NavigationLeftMenu isActive={true} imgSrc={homeBlack} activeImgSrc={homeBlue} menuName="홈" />
-      <NavigationLeftMenu imgSrc={noteBlack} activeImgSrc={homeBlue} menuName="상담노트" />
-      <NavigationLeftMenu imgSrc={adminBlack} activeImgSrc={homeBlue} menuName="계정관리" />
+      <NavigationLeftMenu
+        isActive={true}
+        imgSrc={homeBlack}
+        activeImgSrc={homeBlue}
+        menuName="홈"
+      />
+      <NavigationLeftMenu
+        imgSrc={noteBlack}
+        activeImgSrc={noteBlue}
+        menuName="상담노트"
+      />
+      <NavigationLeftMenu
+        imgSrc={paperPlaneBlack}
+        activeImgSrc={paperPlaneBlue}
+        menuName="케어링노트"
+      />
+      <NavigationLeftMenu
+        imgSrc={adminBlack}
+        activeImgSrc={adminBlue}
+        menuName="계정관리"
+      />
       <div className="absolute bottom-5 w-full">
-        <NavigationLeftMenu imgSrc={settingBlack} activeImgSrc={homeBlue} menuName="설정" />
-        <NavigationLeftMenu imgSrc={questionBlack} activeImgSrc={homeBlue} menuName="도움말" />
+        <NavigationLeftMenu
+          imgSrc={settingBlack}
+          activeImgSrc={homeBlue}
+          menuName="설정"
+        />
+        <NavigationLeftMenu
+          imgSrc={questionBlack}
+          activeImgSrc={homeBlue}
+          menuName="도움말"
+        />
       </div>
     </div>
   );
