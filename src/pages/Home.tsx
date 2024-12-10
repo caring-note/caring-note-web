79f508e6f25768240b39de09b98cea582dbdeb8f
Sanghoon <bshjaws1@gmail.com>
2024-12-10 23:51:20 +0900

    feat: :sparkles: add datepicker component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import DatePickerComponent from "@components/components/ui/datepicker";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
@@ -6,54 +7,55 @@ function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center pt-20 bg-blue-100">
-        <p className="w-full pl-28 text-4xl font-bold text-blue-600">
+        <p className="w-full text-4xl font-bold text-blue-600 pl-28">
           박진완님, <br />
           오늘도 힘찬 하루를 보내세요!{" "}
         </p>
-        <div className="flex flex-row w-full items-center justify-center mt-10 px-24 space-x-5">
-          <div className="w-1/2 h-32 rounded-xl bg-gray-100 border-4 border-white">
+        <div className="flex flex-row items-center justify-center w-full px-24 mt-10 space-x-5">
+          <div className="w-1/2 h-32 bg-gray-100 border-4 border-white rounded-xl">
             <div className="flex flex-row w-full h-full">
-              <div className="flex flex-col items-start justify-center space-y-2 w-1/2 pl-5">
+              <div className="flex flex-col items-start justify-center w-1/2 pl-5 space-y-2">
                 <p>올해 케어링 메세지 공유 수</p>
                 <p className="text-3xl font-bold text-blue-500">{"1,234"}회</p>
               </div>
-              <div className="flex flex-col items-start justify-center space-y-2 w-1/2 pl-5">
+              <div className="flex flex-col items-start justify-center w-1/2 pl-5 space-y-2">
                 <p>올해 상담한 내담자 수</p>
                 <p className="text-3xl font-bold text-blue-500">{"201"}명</p>
               </div>
             </div>
           </div>
-          <div className="w-1/2 h-32 rounded-xl bg-gray-100 border-4 border-white">
+          <div className="w-1/2 h-32 bg-gray-100 border-4 border-white rounded-xl">
             <div className="flex flex-row w-full h-full">
-              <div className="flex flex-col items-start justify-center space-y-2 w-1/2 pl-5">
+              <div className="flex flex-col items-start justify-center w-1/2 pl-5 space-y-2">
                 <p>올해 케어링 메세지 공유 수</p>
                 <p className="text-3xl font-bold text-yellow-500">
                   {"1,234"}회
                 </p>
               </div>
-              <div className="flex flex-col items-start justify-center space-y-2 w-1/2 pl-5">
+              <div className="flex flex-col items-start justify-center w-1/2 pl-5 space-y-2">
                 <p>올해 상담한 내담자 수</p>
                 <p className="text-3xl font-bold text-yellow-500">{"201"}명</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center bg-blue-300 w-full px-20 pt-10">
-          <div className="bg-gray-100 w-full h-screen rounded-xl cursor-pointer">
+        <div className="flex flex-col items-center justify-center w-full px-20 pt-10 bg-blue-300">
+          <div className="w-full h-screen bg-gray-100 cursor-pointer rounded-xl">
             <p
-              className="text-3xl text-center mt-10"
+              className="mt-10 text-3xl text-center"
               onClick={() => {
                 navigate("/consult");
               }}>
               박진완 : 본상담 화면으로 이동 (임시){" "}
             </p>
             <p
-              className="text-3xl text-center mt-10"
+              className="mt-10 text-3xl text-center"
               onClick={() => {
                 navigate("/assistant");
               }}>
               조영호 : 기초상담 화면으로 이동 (임시){" "}
             </p>
+            <DatePickerComponent></DatePickerComponent>
           </div>
         </div>
       </div>
