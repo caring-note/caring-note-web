43aa01c20081109b050d590217e73b410f4c90f7
박진완 <>
2024-12-15 16:34:02 +0900

    feat(본상담화면): :sparkles: 본상담화면 상담 공통 섹션 완성

--- src/pages/consult/Consult.tsx
+++ src/pages/consult/Consult.tsx
@@ -15,9 +15,9 @@ const tabTitle = (text: string, goPage: string) => {
     <p
       className={`${
         activeTab === goPage
-          ? "text-md font-extrabold text-blue-500 border-b-2 border-blue-500"
-          : "text-md font-extrabold text-gray-600"
-      } mr-10 hover:text-blue-500 hover:border-b-2 border-blue-500 cursor-pointer`}
+          ? "text-body2 font-bold text-primary-50 border-b-2 border-primary-50"
+          : "text-body2 font-medium text-grayscale-50"
+      } mr-10 py-3 h-full flex items-center hover:text-primary-50 hover:border-b-2 border-primary-50 cursor-pointer`}
       onClick={() => {
         dispatch(changeActiveTab(goPage));
         navigate(goPage);
@@ -30,7 +30,7 @@ const tabTitle = (text: string, goPage: string) => {
 function Consult() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-start w-full h-fit bg-gray-0 px-8 py-4">
+      <div className="flex flex-col items-center justify-start w-full h-fit px-8 py-4">
         <div className="flex flex-row justify-start w-full h-8 mt-4">
           <img
             src={arrowHeadLeftGray}
@@ -39,10 +39,8 @@ function Consult() {
           />
         </div>
         <div className="flex flex-row items-center justify-start w-full h-8 mt-4 pl-6">
-          <p className="text-4xl text-black font-black">박진완</p>
-          <img src={shareBlue} alt="shareBlue" className="w-8 h-8 ml-4" />
-          <img src={trashcanBlue} alt="trashcanBlue" className="w-8 h-8 ml-4" />
-          <Button _class="ml-4" variant="secondary" onClick={() => {}}>
+          <p className="text-h2 font-bold text-grayscale-100">박진완</p>
+          <Button _class="ml-6" variant="secondary" onClick={() => {}}>
             임시저장
           </Button>
           <Button _class="ml-2" variant="primary" onClick={() => {}}>
@@ -50,15 +48,19 @@ function Consult() {
           </Button>
         </div>
         <div className="flex flex-row items-center justify-start w-full h-8 mt-4 pl-6">
-          <p className="text-md text-black">만 {"29"}세</p>
-          <div className="w-0.5 h-6 bg-gray-300 mx-2" />
-          <p className="text-md text-black">
+          <p className="text-body1 font-medium text-grayscale-70">
+            만 {"29"}세
+          </p>
+          <div className="w-0.5 h-6 bg-grayscale-10 mx-2" />
+          <p className="text-body1 font-medium text-grayscale-70">
             {"고혈압 · 고지혈증 · 뇌혈관질환 · 척추 관절염/신경통 · 호흡기질환"}
           </p>
-          <p className="text-md text-gray-500 px-2">외 {"3"}개의 질병</p>
+          <p className="text-body1 font-medium text-grayscale-30 px-2">
+            외 {"3"}개의 질병
+          </p>
         </div>
       </div>
-      <div className="flex flex-row items-center justify-start w-full h-14 pl-14 my-0 border-t-2 border-gray-200 border-b-2 border-b-gray-300">
+      <div className="flex flex-row items-center justify-start w-full h-auto pl-14 my-0 border-t-2 border-b-2 border-grayscale-5 ">
         {tabTitle("이전 상담 내역", "/consult/pastConsult")}
         {tabTitle("상담카드", "/consult/consultCard")}
         {tabTitle("의약물 기록", "/consult/medicineMemo")}
