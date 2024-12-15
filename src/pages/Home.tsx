b214ac38f637e0f8b17581a86328bb77732f06d9
박진완 <50562139+berrycracker5@users.noreply.github.com>
2024-12-15 18:24:36 +0900

    Feature/consultant home (#49)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,47 +15,168 @@ import {
 import { useNavigate } from "react-router-dom";
 import Tooltip from "@components/Tooltip";
 import Badge from "@components/common/Badge";
+import TableComponent from "@components/common/TableComponent";
+import { GridColDef } from "@mui/x-data-grid";
+import {
+  createDefaultDateColumn,
+  createDefaultNumberColumn,
+  createDefaultTextColumn,
+} from "@utils/TableUtils";
+import moment from "moment";
 
 function Home() {
   const navigate = useNavigate();
 
+  const columns: GridColDef[] = [
+    {
+      field: "col1",
+      headerName: "예약시간",
+      flex: 1,
+      renderCell: (params) => {
+        const formattedDate = params.value
+          ? moment(params.value).format("HH:mm")
+          : null;
+        return (
+          formattedDate || <span className="text-gray-400 italic">HH:mm</span>
+        );
+      },
+    },
+    {
+      ...createDefaultDateColumn({
+        field: "col2",
+        headerName: "상담일자",
+      }),
+    },
+    {
+      ...createDefaultNumberColumn({
+        field: "col3",
+        headerName: "상담횟수",
+        unitName: "회차",
+      }),
+    },
+    {
+      ...createDefaultTextColumn({
+        field: "col4",
+        headerName: "내담자명",
+      }),
+    },
+    {
+      ...createDefaultTextColumn({
+        field: "col5",
+        headerName: "담당약사",
+      }),
+    },
+    {
+      field: "col6",
+      headerName: "담당 할당",
+      flex: 1,
+      renderCell: (params) => {
+        // TODO : 할당 여부에 따라 버튼 diable 처리 (아래는 임시 코드)
+        return parseInt(params.id.toString()) % 2 === 1 ? (
+          <Button variant={"secondary"} onClick={handleClickAssignMe}>
+            나에게 할당
+          </Button>
+        ) : (
+          <Button variant={"secondary"} disabled>
+            할당 완료
+          </Button>
+        );
+      },
+    },
+  ];
+
+  const testRows = [
+    {
+      id: "1",
+      col1: new Date(),
+      col2: new Date(),
+      col3: 1,
+      col4: "김진영",
+      col5: "박진완",
+      col6: "조영호",
+    },
+    {
+      id: "2",
+      col1: new Date(),
+      col2: new Date(),
+      col3: 1,
+      col4: "김진영",
+      col5: "박진완",
+      col6: "조영호",
+    },
+    {
+      id: "3",
+      col1: new Date(),
+      col2: new Date(),
+      col3: 1,
+      col4: "김진영",
+      col5: "박진완",
+      col6: "조영호",
+    },
+  ];
+
+  const handleClickAssignMe = () => {
+    console.log("할당하기 버튼 클릭");
+    // TODO : 할당하기 버튼 클릭 시 확인 Dialog 띄우기
+  };
+
   return (
     <>
       <div className="flex flex-col items-center justify-center pt-20 bg-blue-100">
-        <p className="w-full text-4xl font-bold text-blue-600 pl-28">
+        <p className="w-full text-h2 font-bold text-primary-70 pl-28">
           박진완님, <br />
           오늘도 힘찬 하루를 보내세요!{" "}
         </p>
         <div className="flex flex-row items-center justify-center w-full px-24 mt-10 space-x-5">
-          <div className="w-1/2 h-32 bg-gray-100 border-4 border-white rounded-xl">
+          <div className="w-1/2 h-32 border-2 border-white rounded-xl bg-opacity-60 bg-white">
             <div className="flex flex-row w-full h-full">
               <div className="flex flex-col items-start justify-center w-1/2 pl-5 space-y-2">
-                <p>올해 케어링 메세지 공유 수</p>
-                <p className="text-3xl font-bold text-blue-500">{"1,234"}회</p>
+                <p className="text-body1 font-medium">
+                  올해 케어링 메세지 공유 수
+                </p>
+                <p className="text-h2 font-bold text-primary-50">{"1,234"}회</p>
               </div>
               <div className="flex flex-col items-start justify-center w-1/2 pl-5 space-y-2">
-                <p>올해 상담한 내담자 수</p>
-                <p className="text-3xl font-bold text-blue-500">{"201"}명</p>
+                <p className="text-body1 font-medium">올해 상담한 내담자 수</p>
+                <p className="text-h2 font-bold text-primary-50">{"201"}명</p>
               </div>
             </div>
           </div>
-          <div className="w-1/2 h-32 bg-gray-100 border-4 border-white rounded-xl">
+          <div className="w-1/2 h-32 border-2 border-white rounded-xl bg-opacity-60 bg-white">
             <div className="flex flex-row w-full h-full">
               <div className="flex flex-col items-start justify-center w-1/2 pl-5 space-y-2">
-                <p>올해 케어링 메세지 공유 수</p>
-                <p className="text-3xl font-bold text-yellow-500">
+                <p className="text-body1 font-medium">
+                  올해 케어링 메세지 공유 수
+                </p>
+                <p className="text-h2 font-bold text-secondary-50">
                   {"1,234"}회
                 </p>
               </div>
               <div className="flex flex-col items-start justify-center w-1/2 pl-5 space-y-2">
-                <p>올해 상담한 내담자 수</p>
-                <p className="text-3xl font-bold text-yellow-500">{"201"}명</p>
+                <p className="text-body1 font-medium">올해 상담한 내담자 수</p>
+                <p className="text-h2 font-bold text-secondary-50">{"201"}명</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center w-full px-20 pt-10 bg-blue-300">
-          <div className="w-full h-screen bg-gray-100 cursor-pointer rounded-xl">
+        <div className="flex flex-col items-center justify-center w-full px-24 pt-10">
+          <div className="w-full h-auto bg-white rounded-xl p-10">
+            {/* 실제 코드 */}
+            <div className="w-full h-10 flex flex-row justify-between items-center">
+              <span className="text-h3 font-bold">오늘의 상담 일정</span>
+              <Button variant="secondary">전체 상담 노트 보기</Button>
+            </div>
+            <div className="mt-10">
+              <TableComponent
+                rows={testRows}
+                columns={columns}
+                checkboxSelection={false}
+                onUpdateCell={() => {}}
+                onRowSelectionModelChange={() => {}}
+              />
+            </div>
+
+            {/* 테스트 코드 */}
             <p
               className="mt-10 text-3xl text-center"
               onClick={() => {
@@ -70,6 +191,7 @@ function Home() {
               }}>
               조영호 : 기초상담 화면으로 이동 (임시){" "}
             </p>
+
             <DatePickerComponent></DatePickerComponent>
             <Tabs defaultValue="account" className="w-[400px]">
               <TabsList>
@@ -101,7 +223,7 @@ function Home() {
 
             <Tooltip id="tooltip" text="This is a tooltip" eventType="hover" />
             <Tooltip id="tooltip2" text="This is a tooltip" eventType="click" />
-            
+
             <div className="grid grid-cols-4 gap-4 items-start">
               <div>
                 <Badge variant="filled" size="extra-large" color="primary">
