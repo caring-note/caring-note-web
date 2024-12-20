dca239c3e6c2fd96f76d6329bf7fb1ef167f4993
ohoCoding <youngho7482@gmail.com>
2024-12-20 13:43:47 +0900

    feat: :sparkles: input 컴포넌트 생성 & global css 적용

---
+++ src/components/components/ui/input.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { cn } from "@components/lib/utils";
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-md border-grayscale-30 bg-white px-3 py-2 text-base font-medium border-2",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
