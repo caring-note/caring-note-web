945c2c54c9a6d13b5787b3ef29ec8ce3f8d89068
박진완 <>
2024-12-21 14:09:21 +0900

    feat: :lipstick: 이전 상담 내역 UI 수정사항 반영

---
+++ src/components/consult/PastConsultContainer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface PastConsultContainerProps {
+  variant: "primary" | "secondary";
+  title: string;
+  children?: React.ReactNode;
+}
+
+const PastConsultContainer: React.FC<PastConsultContainerProps> = ({
+  variant,
+  title,
+  children,
+}) => {
+  return (
+    <div className={`w-1/2 h-80 rounded-lg border border-${variant}-30`}>
+      <div className={`bg-${variant}-10 p-4 rounded-t-lg`}>
+        <h2
+          className={`text-subtitle2 font-bold text-${variant}-70 flex items-center`}>
+          {title}
+        </h2>
+      </div>
+      <div className="px-4 pb-4 overflow-y-auto h-64">{children}</div>
+    </div>
+  );
+};
+
+export default PastConsultContainer;
