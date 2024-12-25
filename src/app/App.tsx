9376afa6fb94c8c0a1680c21130d57afeeeef7cf
박진완 <>
2024-12-25 10:52:29 +0900

    build: :heavy_plus_sign: Add tanstack-query lib

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,18 @@
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import store from "../../store";
 import Router from "./Router";
 
+// Tanstack : QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 const App = () => {
-    return(
-        <Provider store={store}>
-            <Router/>
-        </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
+    </Provider>
+  );
 };
 export default App;
