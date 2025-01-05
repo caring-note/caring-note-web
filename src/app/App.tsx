8576511bd6bc9e0fa13da82873e0eaa576bd2d69
Sanghoon <bshjaws1@gmail.com>
2025-01-05 10:55:22 +0900

    feat: :sparkles: add keycloak integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import Router from "./Router";
 import store from "./store";
-
 // Tanstack : QueryClient 인스턴스 생성
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Router />
-      </QueryClientProvider>
+      {/* <ReactKeycloakProvider authClient={keycloak}> */}
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
+      {/* </ReactKeycloakProvider> */}
     </Provider>
   );
 };
