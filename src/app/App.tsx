73029d246d8f7a7b832c3e58416088616379b19e
Sanghoon <bshjaws1@gmail.com>
2025-01-07 23:31:17 +0900

    feat: :wrench: add keycloak configuration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
-import keycloak from "./keycloak";
+import keycloak, { initOptions, onKeycloakEvent } from "./keycloak";
 import Router from "./Router";
 import store from "./store";
 // Tanstack : QueryClient 인스턴스 생성
@@ -10,7 +10,11 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <Provider store={store}>
-      <ReactKeycloakProvider authClient={keycloak}>
+      <ReactKeycloakProvider
+        authClient={keycloak}
+        initOptions={initOptions}
+        onEvent={onKeycloakEvent}
+      >
         <QueryClientProvider client={queryClient}>
           <Router />
         </QueryClientProvider>
