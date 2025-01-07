79482c7c70e2a07953abfbb99626319afca449d4
문혜성 <moonhs97@gmail.com>
2025-01-08 01:27:52 +0900

    feat: feat: auth token 활용 api 호출

--- src/app/keycloak.ts
+++ src/app/keycloak.ts
@@ -1,39 +1,40 @@
 // src/keycloak.js
-import { AuthClientError, AuthClientEvent } from '@react-keycloak/core';
-import Keycloak from 'keycloak-js';
+import { AuthClientError, AuthClientEvent } from "@react-keycloak/core";
+import Keycloak from "keycloak-js";
 
 // Keycloak 설정. 여기서 url, realm, clientId는 위에서 만든 값으로 교체
-const keycloak = new Keycloak({
-  url: 'https://caringnote.co.kr/keycloak',
-  realm: 'caringnote',
-  clientId: 'caringnote',
+export const keycloak = new Keycloak({
+  url: "https://caringnote.co.kr/keycloak",
+  realm: "caringnote",
+  clientId: "caringnote",
 });
 
 export const initOptions = {
-  onLoad: 'login-required',
+  onLoad: "login-required",
   checkLoginIframe: false,
-  pkceMethod: 'S256',
+  pkceMethod: "S256",
 };
 
 // keycloak Event 를 보기 위한 함수 정의
 // keycloak provider 의 onEvent 에 넣어준다.
-export const onKeycloakEvent = (event: AuthClientEvent, error?: AuthClientError) => {
-  console.log('keycloak event ', event, error);
+export const onKeycloakEvent = (
+  event: AuthClientEvent,
+  error?: AuthClientError,
+) => {
+  console.log("keycloak", keycloak);
+  console.log("keycloak.token:", keycloak.token);
   switch (event) {
-    case 'onAuthError':
-      keycloak.login();
+    case "onAuthLogout":
+      keycloak.logout();
       break;
-    case 'onAuthLogout':
-      keycloak.login();
-      break;
-    case 'onAuthSuccess':
-      console.log('onAuthSuccess');
+    case "onAuthSuccess":
+      console.log("onAuthSuccess");
       // useAuth.getState().login(keycloak.token);
       break;
-    case 'onAuthRefreshError':
+    case "onAuthRefreshError":
       keycloak.login();
       break;
-    case 'onTokenExpired':
+    case "onTokenExpired":
       keycloak.updateToken(30);
       break;
   }
