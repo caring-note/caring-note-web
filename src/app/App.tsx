51268b3aa317043d1eeef37c7104e5c216396d50
문혜성 <moonhs97@gmail.com>
2025-01-12 17:20:13 +0900

    build: :arrow_down: install tanstack devtool

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
-import { ReactKeycloakProvider } from "@react-keycloak/web";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider } from "react-redux";
-import keycloak, { initOptions, onKeycloakEvent } from "./keycloak";
-import Router from "./Router";
-import store from "./store";
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import keycloak, { initOptions, onKeycloakEvent } from './keycloak';
+import Router from './Router';
+import store from './store';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 // Tanstack : QueryClient 인스턴스 생성
 const queryClient = new QueryClient();
 
@@ -13,9 +14,9 @@ const App = () => {
       <ReactKeycloakProvider
         authClient={keycloak}
         initOptions={initOptions}
-        onEvent={onKeycloakEvent}
-      >
+        onEvent={onKeycloakEvent}>
         <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
           <Router />
         </QueryClientProvider>
       </ReactKeycloakProvider>
